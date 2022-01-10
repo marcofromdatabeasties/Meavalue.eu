5561102283354119e72ac08225f01babe1a76ef4
marcofromdatabeasties <marco@databeasties.com>
2022-01-10 10:42:36 +0100

    Directory structure

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan 10 10:28:08 2022
 
-@author: ubuntu
+    Main.py at this level does nothing.
+
+    Copyright (C) Mon Jan 10 10:28:08 2022  @author: ubuntu
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 """
 print ("Nothing to do here!")
